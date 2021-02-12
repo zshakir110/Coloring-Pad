953afec704cbebcd6445e76e4fdfbe0f7c43c3d8
zshakir110 <78383982+zshakir110@users.noreply.github.com>
2021-02-12 17:30:56 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,31 @@
+canvas=document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="purple";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth =2;
+ctx.arc(300, 200, 50, 0, 2 * Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+    color = document.getElementById("color").value;
+    console.log(color);
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+    circle(mouse_x, mouse_y)
+}
+
+function circle(mouse_x, mouse_y)
+{ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth =2;
+ctx.arc(mouse_x, mouse_y, 40, 0, 2* Math.PI);
+ctx.stroke();}
+
+function cleararea()
+{
+    ctx.clearRect (0,0,canvas.width,canvas.height);
+
+}
